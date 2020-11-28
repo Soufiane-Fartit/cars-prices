53adc652664447ffd2ecce35170a7efb699b6a4d
Soufiane Fartit <soufiane.fartit@gmail.com>
2020-11-28 21:55:01 +0100

    make_dataset reads raw files and concat them

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,18 +2,34 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+#from dotenv import find_dotenv, load_dotenv
+import glob
+import pandas as pd
 
 
 @click.command()
-@click.argument('input_filepath', default='data/raw', type=click.Path(exists=True))
-@click.argument('output_filepath', default='data/interim', type=click.Path())
+@click.argument('input_filepath', default='data/raw/', type=click.Path(exists=True))
+@click.argument('output_filepath', default='data/interim/', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    list_of_dfs = []
+    all_files = glob.glob(input_filepath + "*.csv")
+
+    for filename in all_files:
+        brand = filename[len(input_filepath):-4]
+        df = pd.read_csv(filename, index_col=None, header=0)
+        #df['brand'] = brand
+        df.insert(0, 'brand', brand)
+        list_of_dfs.append(df)
+    
+    concat = pd.concat(list_of_dfs, axis=0, ignore_index=True)
+    concat.to_csv(output_filepath + 'dataset.csv')
+
 
 
 if __name__ == '__main__':
@@ -25,6 +41,6 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    #load_dotenv(find_dotenv())
 
     main()
